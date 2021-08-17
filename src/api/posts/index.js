453576b993c6d4de618641c4dd01ab4599eacffd
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-08-16 21:46:13 -0700

    auth system finished

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -1,13 +1,22 @@
 import Router from 'koa-router';
 import * as postsCtrl  from './posts.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
 
 const posts = new Router();
 
-
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
-posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
-posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+posts.post('/', checkLoggedIn, postsCtrl.write);
+
+/*
+posts.get('/:id', postsCtrl.getPostById, postsCtrl.read);
+posts.delete('/:id', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.getPostById, postsCtrl.remove);
+posts.patch('/:id', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.getPostById, postsCtrl.update);
+*/
+const post = new Router();
+post.get('/', postsCtrl.read);
+post.delete('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
+post.patch('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
+
+posts.use('/:id', postsCtrl.getPostById, post.routes());
 
 export default posts;
