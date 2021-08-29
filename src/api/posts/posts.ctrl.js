8a6c6fe56f33d29a0ce2a256724919a3f6c8bace
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-08-29 14:54:15 -0700

    sanitizeHtml

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,9 +1,34 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
 import Joi from 'joi';
+import sanitizeHTML from "sanitize-html";
 
 
 const { ObjectId } = mongoose.Types;
+
+const sanitizeOption = {
+    allowedTags: [
+        'h1',
+        'h2',
+        'b',
+        'i',
+        'u',
+        's',
+        'p',
+        'ul',
+        'ol',
+        'li',
+        'blockquote',
+        'a',
+        'img',
+    ],
+    allowedAttributes: {
+        a: ['href', 'name', 'target'],
+        img: ['src'],
+        li: ['class'],
+    },
+    allowedSchemes: ['data', 'http'],
+};
 
 export const getPostById = async (ctx, next) => {
     const { id } = ctx.params;
@@ -33,6 +58,13 @@ export const checkOwnPost = (ctx, next) => {
     return next();
 };
 
+const removeHtmlAndShorten = body => {
+    const filtered = sanitizeHTML(body, {
+        allowedTags: [],
+    });
+    return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`;
+};
+
 
 /* 
 POST /api/posts
@@ -58,7 +90,7 @@ export const write = async ctx => {
     const { title, body, tags } = ctx.request.body;
     const post = new Post({
         title,
-        body,
+        body: sanitizeHTML(body, sanitizeOption),
         tags,
         user: ctx.state.user,
     })
@@ -94,7 +126,7 @@ export const list = async ctx => {
             //.map(post => post.toJSON())
             .map(post => ({
                 ...post,
-                body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`
+                body: removeHtmlAndShorten(post.body),
             }));
     } catch (e) {
         ctx.throw(500, e);
@@ -142,9 +174,12 @@ export const update = async ctx => {
         ctx.body = result.error;
         return;
     };
-
+    const nextData = {...ctx.request.body};
+    if (nextData.body) {
+        nextData.body = sanitizeHTML(nextData.body, sanitizeOption);
+    }
     try {
-        const post = await Post.findByIdAndUpdate(id, ctx.request.body, { new: true }).exec();
+        const post = await Post.findByIdAndUpdate(id, nextData, { new: true }).exec();
         if (!post) {
             ctx.status = 404;
             return;
